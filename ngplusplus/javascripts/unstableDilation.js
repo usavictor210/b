d7230a03fbab32a9c956f4b14f6b4dd0d32ce1ab
Glitch (ngplus2) <none>
2020-02-16 00:29:33 +0000

    🍵🍶 Checkpoint ./ngplusplus/javascripts/unstableDilation.js:7818470/949

---
+++ ngplusplus/javascripts/unstableDilation.js
@@ -0,0 +1,3 @@
+function unstableDilation() {
+ if (!confirm("Unstabilizing time dilation will result in harsher scaling, allowing you to get more TP, but dilation will reset in exchange for Dilation Shards. Are you prepared for this change?"))
+}
