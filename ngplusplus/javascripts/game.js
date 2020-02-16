877b4e3535a4a12caf887bec8cbd8598df74f0a2
Glitch (ngplus2) <none>
2020-02-16 15:21:58 +0000

    💿🚧 Checkpoint ./ngplusplus/index.html:7818470/97 ./ngplusplus/javascripts/game.js:7818470/60 ./ngplusplus/javascripts/core/achievements.js:7818470/13

--- ngplusplus/javascripts/game.js
+++ ngplusplus/javascripts/game.js
@@ -92,7 +92,7 @@ var player = {
     dimensionMultDecrease: 10,
     dimensionMultDecreaseCost: 1e8,
     overXGalaxies: 10,
-    version: 15.2,
+    version: 15.3,
     infDimensionsUnlocked: [false, false, false, false, false, false, false, false],
     infinityPower: new Decimal(1),
     spreadingCancer: 0,
@@ -1429,7 +1429,7 @@ function updateInfCosts() {
         document.getElementById("212desc").textContent = "Currently: "+((Math.pow(player.timeShards.max(2).log2(), 0.005)-1)*100).toFixed(2)+"%"
         document.getElementById("214desc").textContent = "Currently: "+shortenMoney(((calcTotalSacrificeBoost().pow(8)).min("1e46000").times(calcTotalSacrificeBoost().pow(1.1)).div(calcTotalSacrificeBoost())).max(1).min(new Decimal("1e125000")))+"x"
 
-        if (player.etercreq !== 1) document.getElementById("ec1unl").innerHTML = "Eternity Challenge 1<span>Requirement: "+(ECTimesCompleted("eterc1")+1)*20000+" Eternities<span>Cost: 30 Time Theorems"
+        if (player.etercreq !== 1) document.getElementById("ec1unl").innerHTML = "Eternity Challenge 1<span>Requirement: "+(ECTimesCompleted("eterc1")+1)*200+" Eternities<span>Cost: 30 Time Theorems"
         else document.getElementById("ec1unl").innerHTML = "Eternity Challenge 1<span>Cost: 30 Time Theorems"
         if (player.etercreq !== 2) document.getElementById("ec2unl").innerHTML = "Eternity Challenge 2<span>Requirement: "+(1300+(ECTimesCompleted("eterc2")*150))+" Tickspeed upgrades gained from time dimensions<span>Cost: 35 Time Theorems"
         else document.getElementById("ec2unl").innerHTML = "Eternity Challenge 2<span>Cost: 35 Time Theorems"
@@ -2296,7 +2296,7 @@ function setAchieveTooltip() {
     let speed = document.getElementById("Ludicrous Speed")
     let speed2 = document.getElementById("I brake for nobody")
     let overdrive = document.getElementById("MAXIMUM OVERDRIVE")
-    let minute = document.getElementById("Minute of infinity")
+    let minute = document.getElementById("4.3333 minutes of Infinity")
     let infiniteIP = document.getElementById("Can you get infinite IP?")
     let over9000 = document.getElementById("IT'S OVER 9000")
     let dawg = document.getElementById("Yo dawg, I heard you liked infinities...")
@@ -3982,7 +3982,7 @@ function canUnlockEC(idx, cost, study, study2) {
 
     switch(idx) {
         case 1:
-        if (player.eternities >= 20000+(ECTimesCompleted("eterc1")*20000)) return true
+        if (player.eternities >= 200+(ECTimesCompleted("eterc1")*200)) return true
         break;
 
         case 2:
@@ -4989,7 +4989,7 @@ setInterval(function() {
     if (player.money.gte("1e35000")) giveAchievement("I got a few to spare")
     if (player.infinityPower.gt(1)) giveAchievement("A new beginning.");
     if (player.infinityPower.gt(1e6)) giveAchievement("1 million is a lot"); //TBD
-    if (player.infinityPower.gt(1e260)) giveAchievement("Minute of infinity"); //TBD
+    if (player.infinityPower.gt(1e260)) giveAchievement("4.3333 minutes of Infinity"); //TBD
     if (player.totalTickGained >= 308) giveAchievement("Infinite time");
     if (player.totalTickGained >= 1e6) giveAchievement("GAS GAS GAS");
     if (player.firstPow >= 1e31) giveAchievement("I forgot to nerf that")
