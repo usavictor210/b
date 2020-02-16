bcaaa4142831f01525f22c992273d3db0bef4ea8
Glitch (ngplus2) <none>
2020-02-16 00:41:32 +0000

    👏🚂 Checkpoint ./ngplusplus/javascripts/game.js:7818470/12 ./ngplusplus/javascripts/unstableDilation.js:7818470/1118 ./ngplusplus/index.html:7818470/104

--- ngplusplus/javascripts/unstableDilation.js
+++ ngplusplus/javascripts/unstableDilation.js
@@ -1,3 +1,19 @@
-function unstableDilation() {
+/*function unstableDilation() {
+let usGain = (Math.floor(player.dilation.tachyonParticles.log(5)/10))
  if (!confirm("Unstabilizing time dilation will result in harsher scaling, allowing you to get more TP, but dilation will reset in exchange for Dilation Shards. Are you prepared for this change?"))
+        player.dilation.studies = player.dilation.studies,
+        player.dilation.active = false,
+        player.dilation.tachyonParticles = new Decimal(0),
+        player.dilation.dilatedTime = new Decimal(0),
+        player.dilation.nextThreshold = new Decimal(1000),
+        player.dilation.freeGalaxies = 0,
+        player.dilation.upgrades = [],
+        player.dilation.rebuyables = {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0
+        }
+        player.dilation.unstableShards.add(usGain)
 }
+*/
