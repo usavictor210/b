9539105f132f9fc996b4c0fde0a0d21b31ea1761
Glitch (ngplus2) <none>
2020-02-18 16:59:12 +0000

    🚥🐯 Checkpoint ./ngplusplus/javascripts/core/meta_dimension.js:7818470/352

--- ngplusplus/javascripts/core/meta_dimension.js
+++ ngplusplus/javascripts/core/meta_dimension.js
@@ -154,7 +154,7 @@ function metaBoost () {
 function metaGalaxyCost() {
   return {
     tier: 8,
-    amount: Math.max(40, -5 + 50 * player.meta.galaxy)
+    amount: Math.max(40, -5 + 50 * player.meta.galaxy + ((player.meta.galaxy-6)*10))
   }
 }
 
