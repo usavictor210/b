adb677609f949093bc7839b24a6e79d33b1bf21b
Glitch (ngplus2) <none>
2020-02-18 04:21:43 +0000

    📌🚨 Checkpoint ./ngplusplus/javascripts/unstableDilation.js:7818470/940

--- ngplusplus/javascripts/unstableDilation.js
+++ ngplusplus/javascripts/unstableDilation.js
@@ -1,7 +1,7 @@
 function unstableDilation() {
-var usGain = new Decimal(((player.dilation.tachyonParticles.log(10)/10)).floor).max(1)
- if (!player.dilationStudy.includes(5) || player.dilation.dilatedTime.lt(9.99e99)) return
- if (!confirm("Unstabilizing time dilation will result in harsher scaling, allowing you to get more TP, but dilation will reset in exchange for Dilation Shards. Are you prepared for this change?"))
+var usGain = new Decimal(((player.dilation.tachyonParticles.log(10)/15))).max(1).floor()
+ if (!player.dilation.studies.includes(6) || player.dilation.dilatedTime.lt(9.99e99)) return
+ if (confirm("Unstabilizing time dilation will result in harsher scaling, allowing you to get more TP, but dilation will reset in exchange for Dilation Shards. Are you prepared for this change?"))
         player.dilation.studies = player.dilation.studies,
         player.dilation.active = false,
         player.dilation.tachyonParticles = new Decimal(0),
