571372bf7dc7a9df5b2381553b559f01e63ca724
Glitch (ngplus2) <none>
2020-02-19 01:15:57 +0000

    👩‍❤️‍💋‍👩🏯 Checkpoint ./ngplusplus/javascripts/core/quantum.js:9422105/401 ./ngplusplus/javascripts/core/load_functions.js:9422105/97547

--- ngplusplus/javascripts/core/quantum.js
+++ ngplusplus/javascripts/core/quantum.js
@@ -410,8 +410,3 @@ function toggleQuantumConf() {
         document.getElementById("quantumconf").textContent = "Quantum confirmation ON"
     }
 }
-
-if (player.dilation.tachyonParticles !== 0 || player.quantum.times !== 0 ) document.getElementById("dilationconf").style.display = "inline-block"
-else document.getElementById("dilationconf").style.display = "inline-block"
-if (player.quantum.times !== 0 ) document.getElementById("quantumconf").style.display = "inline-block"
-else document.getElementById("quantumconf").style.display = "inline-block"
