5b0bfbaf5b24fddce1b48e45ec00aa52a04f808e
Glitch (ngplus2) <none>
2020-02-20 15:36:11 +0000

    🏇🎐 Checkpoint ./ngplusplus/javascripts/game.js:7818470/8 ./ngplusplus/javascripts/unstableDilation.js:7818470/190

--- ngplusplus/javascripts/unstableDilation.js
+++ ngplusplus/javascripts/unstableDilation.js
@@ -42,12 +42,12 @@ function timeMultUpg(x, check) {
   if (x === 1) {
       if (player.achievements.includes("r151")) {
         y = new Decimal(Decimal.pow(0.5 * player.totalTimePlayed / 360, 0.15).pow(timeLeaperMult().log10()))
-    } else y = Decimal.pow(0.5 * player.totalTimePlayed / 600, 0.15).toFixed(2)
+    } else y = new Decimal (Decimal.pow(0.5 * player.totalTimePlayed / 600, 0.15).toFixed(2))
   }
   if (x === 2) {
     if (player.achievements.includes("r151")) {
       y = new Decimal ((Decimal.max(Math.pow(player.thisInfinityTime / 50, 0.4)+1, 1).pow((timeLeaperMult()).times(player.thisInfinityTime), 1.05).log10()))
-    } else y = Decimal.max(Math.pow(player.thisInfinityTime / 2400, 0.25), 1).toFixed(2)
+    } else y = new Decimal (Decimal.max(Math.pow(player.thisInfinityTime / 2400, 0.25), 1).toFixed(2))
   }
   if (x === 3) {
     if (player.achievements.includes("r151")) {
