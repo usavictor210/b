d5b1b12cbb16ce2bef05e8b2ba3a689145e82fdc
Glitch (ngplus2) <none>
2020-02-21 15:36:17 +0000

    🍋🐞 Checkpoint ./ngplusplus/index.html:7818470/4 ./ngplusplus/javascripts/unstableDilation.js:7818470/705

--- ngplusplus/javascripts/unstableDilation.js
+++ ngplusplus/javascripts/unstableDilation.js
@@ -1,6 +1,6 @@
 function unstableDilation() {
 var usGain = new Decimal(((player.dilation.tachyonParticles.log(10).divide(15)))).max(1).floor()
- if (!player.dilation.studies.includes(6) || player.dilation.dilatedTime.lt(9.99e99)) return
+ if (!player.dilation.studies.includes(6) || player.dilation.dilatedTime.lt(9.99e99) || player.quantum.times === 0) return
  if (confirm("Unstabilizing time dilation will result in harsher scaling, allowing you to get more TP, but dilation will reset in exchange for Dilation Shards. Are you prepared for this change?")) {
         eternity(true)
         player.dilation.studies = player.dilation.studies,
@@ -27,6 +27,9 @@ var usGain = new Decimal(((player.dilation.tachyonParticles.log(10).divide(15)))
 function checkUnstableDilationButton() {
   if (player.dilation.dilatedTime.gte(9.99e99) && !player.quantum.times === 0) document.getElementById("enabledilation2").style.display = "inline-block"
   else document.getElementById("enabledilation2").style.display = "none"
+  if (!player.dilation.studies.includes(6) || player.dilation.dilatedTime.lt(9.99e99) || player.quantum.times === 0) {
+  document.getElementById("enabledilation2").textContext = "Go quantum and have at least 1e100 DT to unstabilize dilation."
+  } else document.getElementById("enabledilation2").textContext = "Unstabilize dilation."
 }
 
 
