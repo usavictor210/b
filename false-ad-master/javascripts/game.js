b91e65bfe9f3f48816953286bfc96f5772e80266
Victor <32530025+usavictor210@users.noreply.github.com>
2020-07-12 22:00:41 -0400

    Update game.js

--- false-ad-master/javascripts/game.js
+++ false-ad-master/javascripts/game.js
@@ -4976,7 +4976,7 @@ function startInterval() {
         document.getElementById("infinitybtn").style.display = "none";
         document.getElementById("challengesbtn").style.display = "none";
 
-        if (player.money.gte(player.challengeTarget) && (player.bestInfinityTime > 600 || ((!player.currentChallenge.name == "") && !infinityBrokenInCurrentChallenge()))) {
+        if (player.money.gte(Number.MAX_VALUE) && (((player.currentChallenge != "" && player.money.gte(player.challengeTarget)) && !player.options.retryChallenge) || (player.bestInfinityTime > 600 && !player.break))) {
             document.getElementById("dimensionsbtn").style.display = "none";
             document.getElementById("optionsbtn").style.display = "none";
             document.getElementById("statisticsbtn").style.display = "none";
